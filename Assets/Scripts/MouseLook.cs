d20f927539f918eaa0b50d3aec0d9b30b2e944bc
Zack <dellaroccozack@gmail.com>
2021-02-16 23:08:05 -0500

    Initial Commit
    Has player movement code and a dev test level.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float sens = 100f;
+    public Transform playerBody;
+
+    float xRot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float groundDist = 0.4f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+
+    public LayerMask groundMask;
+
+    Vector3 vel;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+
+        if(isGrounded && vel.y < 0)
+        {
+            vel.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            vel.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        vel.y += gravity * Time.deltaTime;
+
+        controller.Move(vel * Time.deltaTime);
+    }
+}
